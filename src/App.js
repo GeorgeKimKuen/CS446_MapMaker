5c5faa012ed46ddde90d48e86cbdd7457e1d3ae2
Alice Williams <alice.williams.tech@gmail.com>
2022-10-30 18:40:08 -0700

    Code spacing cleanup

--- src/App.js
+++ src/App.js
@@ -3,78 +3,78 @@ import Menu from "./components/Menu";
 import "./App.css";
 
 function App() {
-const canvasRef = useRef(null);
-const ctxRef = useRef(null);
-const [isDrawing, setIsDrawing] = useState(false);
-const [lineWidth, setLineWidth] = useState(5);
-const [lineColor, setLineColor] = useState("black");
-const [lineOpacity, setLineOpacity] = useState(0.1);
+    const canvasRef = useRef(null);
+    const ctxRef = useRef(null);
+    const [isDrawing, setIsDrawing] = useState(false);
+    const [lineWidth, setLineWidth] = useState(5);
+    const [lineColor, setLineColor] = useState("black");
+    const [lineOpacity, setLineOpacity] = useState(0.1);
 
-// Initialization when the component
-// mounts for the first time
-useEffect(() => {
-	const canvas = canvasRef.current;
-	const ctx = canvas.getContext("2d");
-	ctx.lineCap = "round";
-	ctx.lineJoin = "round";
-	ctx.globalAlpha = lineOpacity;
-	ctx.strokeStyle = lineColor;
-	ctx.lineWidth = lineWidth;
-	ctxRef.current = ctx;
-}, [lineColor, lineOpacity, lineWidth]);
+    // Initialization when the component
+    // mounts for the first time
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext("2d");
+        ctx.lineCap = "round";
+        ctx.lineJoin = "round";
+        ctx.globalAlpha = lineOpacity;
+        ctx.strokeStyle = lineColor;
+        ctx.lineWidth = lineWidth;
+        ctxRef.current = ctx;
+    }, [lineColor, lineOpacity, lineWidth]);
 
-// Function for starting the drawing
-const startDrawing = (e) => {
-	ctxRef.current.beginPath();
-	ctxRef.current.moveTo(
-	e.nativeEvent.offsetX,
-	e.nativeEvent.offsetY
-	);
-	setIsDrawing(true);
-};
+    // Function for starting the drawing
+    const startDrawing = (e) => {
+        ctxRef.current.beginPath();
+        ctxRef.current.moveTo(
+        e.nativeEvent.offsetX,
+        e.nativeEvent.offsetY
+        );
+        setIsDrawing(true);
+    };
 
-// Function for ending the drawing
-const endDrawing = () => {
-	ctxRef.current.closePath();
-	setIsDrawing(false);
-};
+    // Function for ending the drawing
+    const endDrawing = () => {
+        ctxRef.current.closePath();
+        setIsDrawing(false);
+    };
 
-const draw = (e) => {
-  if (!isDrawing) {
-    return;
-  }
-  ctxRef.current.lineTo(
-    e.nativeEvent.offsetX,
-    e.nativeEvent.offsetY
-  );
+    const draw = (e) => {
+      if (!isDrawing) {
+        return;
+      }
+      ctxRef.current.lineTo(
+        e.nativeEvent.offsetX,
+        e.nativeEvent.offsetY
+      );
 
-  ctxRef.current.stroke();
-};
+      ctxRef.current.stroke();
+    };
 
-return (
-    <div className="App">
-      <div id="content" class="solid">
+    return (
+        <div className="App">
+          <div id="content" class="solid">
 
-        <div className="draw-area">
-        <h1>Map Maker</h1>
+            <div className="draw-area">
+            <h1>Map Maker</h1>
 
-          <Menu
-            setLineColor={setLineColor}
-            setLineWidth={setLineWidth}
-            setLineOpacity={setLineOpacity}
-          />
-          <canvas
-            onMouseDown={startDrawing}
-            onMouseUp={endDrawing}
-            onMouseMove={draw}
-            ref={canvasRef}
-            width={`1920px`}
-            height={`930px`}
-          />
+              <Menu
+                setLineColor={setLineColor}
+                setLineWidth={setLineWidth}
+                setLineOpacity={setLineOpacity}
+              />
+              <canvas
+                onMouseDown={startDrawing}
+                onMouseUp={endDrawing}
+                onMouseMove={draw}
+                ref={canvasRef}
+                width={`1920px`}
+                height={`930px`}
+              />
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
